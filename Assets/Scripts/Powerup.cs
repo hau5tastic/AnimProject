5fb89029a79194c6cdfd5628629ae81c5577d4d4
hau5tastic <hau5tastic@gmail.com>
2016-04-09 16:39:18 -0400

    added unity project

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+    public Vector3 rotationSpeed = new Vector3(0, 0, 200);
+
+    public enum Type
+    {
+        Damage,
+        MoveSpeed
+    };
+
+    int damageBoost = 20;
+    float moveSpeedBoost = 1.5f;
+
+    Type type;
+
+	// Use this for initialization
+	void Start () {
+        int typeSelect = Random.Range(1, 2);
+
+        switch (typeSelect)
+            {
+            case 1:
+                type = Type.Damage;
+                break;
+            case 2:
+                type = Type.MoveSpeed;
+                break;
+            }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotationSpeed);
+        Debug.Log("rotating");
+    }
+}
