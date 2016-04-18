be00ebc3a4f469e0774609910ee5fd5b98ab45ef
Hologuardian <namaeskram@gmail.com>
2016-04-18 11:37:58 -0400

    Added dynamic overhead camera script to follow players

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    const float r3 = 1.7320508f;
+    public float max = 0;
+    public GameObject[] targets;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 avg = new Vector3(0,0,0);
+        int iter = 0;
+        max = 0;
+        Vector3 cameraXY = new Vector3(transform.position.x, 0, transform.position.z);
+	    foreach(GameObject obj in targets)
+        {
+            avg += obj.transform.position;
+            iter++;
+            Vector3 objXY = new Vector3(obj.transform.position.x, 0, obj.transform.position.z);
+            float dist = (cameraXY - objXY).magnitude;
+            if(dist > max)
+                max = dist;
+        }
+        avg /= iter;
+        transform.position = new Vector3(avg.x, max * r3, avg.z);
+	}
+}
